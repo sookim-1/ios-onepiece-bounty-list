f33f479a7f96beecaa7b0cc01ed25f81d20468a9
Sookim18 <scstnghks@naver.com>
2021-04-23 03:41:15 +0900

    Feat : 뷰속성을 활용한 애니메이션 구현 - 뷰속성을 활용하여 애니메이션 구현

--- ios-onepiece-bounty-list/View/DetailViewController.swift
+++ ios-onepiece-bounty-list/View/DetailViewController.swift
@@ -12,8 +12,6 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
-    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
-    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
     
@@ -27,19 +25,25 @@ class DetailViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         showAnimation()
-        
     }
     
     private func prepareAnimation() {
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     private func showAnimation() {
-        nameLabelCenterX.constant = 0
-        bountyLabelCenterX.constant = 0
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+                self.nameLabel.transform = CGAffineTransform.identity
+                self.nameLabel.alpha = 1
+            }, completion: nil)
         
-        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: { self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+                self.bountyLabel.transform = CGAffineTransform.identity
+                self.bountyLabel.alpha = 1
             }, completion: nil)
         
         UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
